b16715f763ccb3a418c3ceab14400005641150d7
dangrier <daniel.grier@gmail.com>
2017-12-25 11:58:30 +1000

    Added basic implementations for both account endpoints, and for markets.

--- btcmarkets.go
+++ btcmarkets.go
@@ -33,6 +33,10 @@ type Client struct {
 // BTC Markets directly. Where strings are used, base-64 encoding is implied -
 // otherwise []byte will represent the actual bytes.
 func NewClient(k, s string) (*Client, error) {
+	if k == "" || s == "" {
+		return nil, errors.New("No key information supplied")
+	}
+
 	b64 := base64.StdEncoding
 
 	binSecret, err := b64.DecodeString(s)
@@ -46,8 +50,14 @@ func NewClient(k, s string) (*Client, error) {
 	}, nil
 }
 
-func (c *Client) authenticateRequestMessage(endpoint string, timestamp int64, requestBody string) string {
-	constructedMessage := fmt.Sprintf("%s\n%d\n%s", endpoint, timestamp, requestBody)
+func (c *Client) authenticateRequestMessage(endpoint string, timestamp time.Time, requestBody string) string {
+	// Convert time.Time into int64 milliseconds timestamp
+	ts := timestamp.UnixNano() / int64(time.Millisecond)
+
+	var constructedMessage string
+
+	constructedMessage = fmt.Sprintf("%s\n%d\n%s", endpoint, ts, requestBody)
+
 	b64 := base64.StdEncoding
 
 	mac := hmac.New(sha512.New, c.secret)
@@ -59,7 +69,7 @@ func (c *Client) authenticateRequestMessage(endpoint string, timestamp int64, re
 
 // String is present to implement the Stringer interface for the Client type.
 func (c *Client) String() string {
-	return fmt.Sprintf("{%s}", c.apikey)
+	return fmt.Sprintf("Client:{%s}", c.apikey)
 }
 
 /*
@@ -102,7 +112,7 @@ An optional GET parameter "since" can be used against the trades endpoint
 
 // MarketTick implements the /market/[instrument]/[currency]/tick endpoint.
 func (c *Client) MarketTick(instrument Instrument, currency Currency) (MarketTickData, error) {
-	res, err := http.Get(fmt.Sprintf("%s/market/%s/%s/tick", APILocation, instrument, currency))
+	res, err := netHTTPClient.Get(fmt.Sprintf("%s/market/%s/%s/tick", APILocation, instrument, currency))
 	if err != nil {
 		return MarketTickData{}, err
 	}
@@ -122,6 +132,58 @@ func (c *Client) MarketTick(instrument Instrument, currency Currency) (MarketTic
 	return mtd, nil
 }
 
+// MarketOrderbook implements the /market/[instrument]/[currency]/orderbook endpoint.
+func (c *Client) MarketOrderbook(instrument Instrument, currency Currency) (MarketOrderbookData, error) {
+	res, err := netHTTPClient.Get(fmt.Sprintf("%s/market/%s/%s/orderbook", APILocation, instrument, currency))
+	if err != nil {
+		return MarketOrderbookData{}, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return MarketOrderbookData{}, err
+	}
+
+	var mod MarketOrderbookData
+	err = json.Unmarshal(body, &mod)
+	if err != nil {
+		return MarketOrderbookData{}, err
+	}
+
+	return mod, nil
+}
+
+// MarketTrades implements the /market/[instrument]/[currency]/trades endpoint.
+//
+// "since" is an optional parameter which, when greater than 0 will only get MarketTrades
+// which occurred since the supplied trade ID.
+func (c *Client) MarketTrades(instrument Instrument, currency Currency, since int64) (MarketTradesData, error) {
+	var uriSince string
+	if since > 0 {
+		uriSince = fmt.Sprintf("?since=%d", since)
+	}
+
+	res, err := netHTTPClient.Get(fmt.Sprintf("%s/market/%s/%s/trades%s", APILocation, instrument, currency, uriSince))
+	if err != nil {
+		return MarketTradesData{}, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return MarketTradesData{}, err
+	}
+
+	var mtd MarketTradesData
+	err = json.Unmarshal(body, &mtd)
+	if err != nil {
+		return MarketTradesData{}, err
+	}
+
+	return mtd, nil
+}
+
 /*
 ACCOUNT [HTTP GET ENDPOINTS]
 
@@ -129,6 +191,85 @@ Endpoints:
 /account/balance
 /account/:instrument/:currency/tradingfee
 */
+
+// AccountBalance implements the /account/balance endpoint.
+func (c *Client) AccountBalance() ([]AccountBalanceData, error) {
+	ts := time.Now()
+	signature := c.authenticateRequestMessage("/account/balance", ts, "")
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/account/balance", APILocation), nil)
+	if err != nil {
+		fmt.Println("error creating account balance request: " + err.Error())
+		return []AccountBalanceData{}, errors.New("error creating account balance request: " + err.Error())
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Accept-Charset", "UTF-8")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("apikey", c.apikey)
+	req.Header.Set("timestamp", fmt.Sprintf("%d", ts.UnixNano()/int64(time.Millisecond)))
+	req.Header.Set("signature", signature)
+
+	res, err := netHTTPClient.Do(req)
+	if err != nil {
+		fmt.Println("error receiving account balance response: " + err.Error())
+		return []AccountBalanceData{}, errors.New("error receiving account balance response: " + err.Error())
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return []AccountBalanceData{}, errors.New("error reading account balance response body: " + err.Error())
+	}
+
+	var accbal []AccountBalanceData
+	err = json.Unmarshal(body, &accbal)
+	if err != nil {
+		return []AccountBalanceData{}, errors.New("error unmarshalling account balance body: " + err.Error())
+	}
+
+	return accbal, nil
+}
+
+// AccountTradingFee implements the /account/:instrument/:currency/tradingfee endpoint.
+func (c *Client) AccountTradingFee(instrument Instrument, currency Currency) (AccountTradingFeeData, error) {
+	ts := time.Now()
+	ep := fmt.Sprintf("/account/%s/%s/tradingfee", instrument, currency)
+	signature := c.authenticateRequestMessage(ep, ts, "")
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s%s", APILocation, ep), nil)
+	if err != nil {
+		fmt.Println("error creating account trading fee request: " + err.Error())
+		return AccountTradingFeeData{}, errors.New("error creating account trading fee request: " + err.Error())
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Accept-Charset", "UTF-8")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("apikey", c.apikey)
+	req.Header.Set("timestamp", fmt.Sprintf("%d", ts.UnixNano()/int64(time.Millisecond)))
+	req.Header.Set("signature", signature)
+
+	res, err := netHTTPClient.Do(req)
+	if err != nil {
+		fmt.Println("error receiving account trading fee response: " + err.Error())
+		return AccountTradingFeeData{}, errors.New("error receiving account trading fee response: " + err.Error())
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return AccountTradingFeeData{}, errors.New("error reading account trading fee response body: " + err.Error())
+	}
+
+	var trafee AccountTradingFeeData
+	err = json.Unmarshal(body, &trafee)
+	if err != nil {
+		return AccountTradingFeeData{}, errors.New("error unmarshalling account trading fee body: " + err.Error())
+	}
+
+	return trafee, nil
+}
 
 /*
 FUNDTRANSFER [MIXED ENDPOINTS]
@@ -139,3 +280,8 @@ Endpoints:
 /fundtransfer/history
 
 */
+
+// FundtransferHistory implements the /fundtransfer/history endpoint.
+func (c *Client) FundtransferHistory() {
+	// Not implemented in API - preview only!
+}
--- semantics.go
+++ semantics.go
@@ -5,18 +5,24 @@ const APILocation = "https://api.btcmarkets.net"
 type Currency string
 
 const (
-	AUD Currency = "AUD"
+	CurrencyAUD        Currency = "AUD"
+	CurrencyBcash      Currency = "BCH"
+	CurrencyBitcoin    Currency = "BTC"
+	CurrencyEthereum   Currency = "ETH"
+	CurrencyEthClassic Currency = "ETC"
+	CurrencyLitecoin   Currency = "LTC"
+	CurrencyRipple     Currency = "XRP"
 )
 
 type Instrument string
 
 const (
-	Bcash      Instrument = "BCH"
-	Bitcoin    Instrument = "BTC"
-	Ethereum   Instrument = "ETH"
-	EthClassic Instrument = "ETC"
-	Litecoin   Instrument = "LTC"
-	Ripple     Instrument = "XRP"
+	InstrumentBcash      Instrument = "BCH"
+	InstrumentBitcoin    Instrument = "BTC"
+	InstrumentEthereum   Instrument = "ETH"
+	InstrumentEthClassic Instrument = "ETC"
+	InstrumentLitecoin   Instrument = "LTC"
+	InstrumentRipple     Instrument = "XRP"
 )
 
 type OrderSide string
@@ -41,4 +47,32 @@ type MarketTickData struct {
 	Instrument Instrument `json:"instrument"`
 	Timestamp  int64      `json:"timestamp"`
 	Volume     float64    `json:"volume24h"`
+}
+
+type MarketOrderbookData struct {
+	Bids       [][]float64 `json:"bids"`
+	Asks       [][]float64 `json:"asks"`
+	Currency   Currency    `json:"currency"`
+	Instrument Instrument  `json:"instrument"`
+	Timestamp  int64       `json:"timestamp"`
+}
+
+type MarketTradesData []TradeData
+
+type TradeData struct {
+	TradeID   int64   `json:"tid"`
+	Amount    float64 `json:"amount"`
+	Price     float64 `json:"price"`
+	Timestamp int64   `json:"date"`
+}
+
+type AccountBalanceData struct {
+	Currency Currency `json:"currency"`
+	Balance  float64  `json:"balance"`
+	Pending  float64  `json:"pendingFunds"`
+}
+
+type AccountTradingFeeData struct {
+	TradingFee   float64 `json:"tradingFeeRate"`
+	Volume30Days float64 `json:"volume30Day"`
 }
